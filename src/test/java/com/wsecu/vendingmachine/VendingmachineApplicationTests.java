6c8c7f49d645498472a34a77b5c44db503a549a3
Kabali Balasubramanian <132620231+kabalibala@users.noreply.github.com>
2023-05-05 18:30:37 -0700

    Initial Commit

---
+++ src/test/java/com/wsecu/vendingmachine/VendingmachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wsecu.vendingmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendingmachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
